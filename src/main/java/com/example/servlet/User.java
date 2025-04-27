79f850f9578d8c6255a0961544eac9827e50e3d9
sdybybhon <artem563732@mail.ru>
2025-04-28 01:21:09 +0500

    Connect DB with JBDC

--- src/main/java/com/example/servlet/User.java
+++ src/main/java/com/example/servlet/User.java
@@ -3,18 +3,17 @@ package com.example.servlet;
 import java.io.Serializable;
 import java.nio.file.Paths;
 
-public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class User {
     private final String username;
     private final String password;
     private final String email;
     private final String homeDirectory;
 
-    public User(String username, String password, String email, String homeBasePath) {
+    public User(String username, String password, String email, String homeDirectory) {
         this.username = username;
         this.password = password;
         this.email = email;
-        this.homeDirectory = Paths.get(homeBasePath, username).toString();
+        this.homeDirectory = homeDirectory;
     }
 
     public String getUsername() { return username; }
@@ -22,3 +21,4 @@ public class User implements Serializable {
     public String getEmail() { return email; }
     public String getHomeDirectory() { return homeDirectory; }
 }
+
